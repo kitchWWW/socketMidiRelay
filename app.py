63471277034d76543f65a32b29123101aa221f56
kitchWWW <brian.e2014@gmail.com>
2022-05-17 22:19:17 -0400

    yep

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 from flask import send_file
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/socketMidiRelay',static_folder='socketMidiRelay')
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
@@ -17,15 +17,15 @@ def handle_my_custom_event(json):
     emit('my response', json, broadcast=True)
 
 
-@app.route('/midi/')
-def index():
-    print("sending index")
-    return send_file('static/index.html')
+# @app.route('/midi/')
+# def index():
+#     print("sending index")
+#     return send_file('static/index.html')
 
-@app.route('/midi/go.js')
-def js():
-    print("sending index")
-    return send_file('static/go.js')
+# @app.route('/midi/go.js')
+# def js():
+#     print("sending index")
+#     return send_file('static/go.js')
 
 
 
